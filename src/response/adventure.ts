38e00054b9800e31cb6557eb49d4389222016eab
ALemonX <setup@alemonjs.local>
2026-08-12 02:05:54 +0800

    123

--- src/response/adventure.ts
+++ src/response/adventure.ts
@@ -32,8 +32,9 @@ const appendCombatLog = (markdown: ReturnType<typeof Format.createMarkdown>, tex
     if (line === '————') { markdown.addNewline().addNewline().addText('————————').addNewline().addNewline(); continue; }
     if (line.startsWith('§')) markdown.addBlockquote(line.slice(1).replaceAll('$', '\\$').replaceAll('#', '\\#')).addNewline();
     else if (line.startsWith('➥') || line.startsWith('$')) markdown.addBlockquote(line.replaceAll('$', '\\$').replaceAll('#', '\\#')).addNewline();
+    else if (line.startsWith('#')) markdown.addBlockquote(line.replaceAll('#', '\\#')).addNewline();
     else if (line.startsWith('➤')) markdown.addNewline().addText(line).addNewline();
-    else markdown.addText(line.startsWith('#') ? line.replaceAll('#', '\\#') : line).addNewline();
+    else markdown.addText(line).addNewline();
   }
   return markdown;
 };
